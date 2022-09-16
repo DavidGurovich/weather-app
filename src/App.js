ad86eb50b06ca21186f726c09877028760065731
David Gurovich <david.gurovich@outlook.com>
2022-09-16 16:12:07 -0700

    weather app initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=6a514bdeb53691e0aed7971b303f3d9b`
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
     </div>
   );
 }
